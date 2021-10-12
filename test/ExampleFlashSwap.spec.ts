fb999bd117f66e22acda409b9e51c4a73827f3d8
Sitthiphong Muntrima <sitthiphong@ookbee.com>
2021-10-12 18:49:03 +0700

    🔥 Remove log in ExampleFlashSwap.spec.ts

--- test/ExampleFlashSwap.spec.ts
+++ test/ExampleFlashSwap.spec.ts
@@ -13,7 +13,8 @@ import ExampleFlashSwap from '../artifacts/contracts/examples/ExampleFlashSwap.s
 chai.use(solidity)
 
 const overrides = {
-  gasLimit: 9999999
+  gasLimit: 9999999,
+  gasPrice: 0
 }
 
 describe('ExampleFlashSwap', () => {
@@ -46,9 +47,6 @@ describe('ExampleFlashSwap', () => {
       [fixture.factoryV2.address, fixture.factoryV1.address, fixture.router.address],
       overrides
     )
-
-    console.log("INIT_CODE_HASH: %s", await fixture.factoryV2.INIT_CODE_HASH())
-    
   })
 
   it('uniswapV2Call:0', async () => {
@@ -70,7 +68,7 @@ describe('ExampleFlashSwap', () => {
     await WETHPair.mint(wallet.address, overrides)
 
     const balanceBefore = await WETHPartner.balanceOf(wallet.address)
-    console.log("balanceBefore: %s", balanceBefore)
+
     // now, execute arbitrage via uniswapV2Call:
     // receive 1 ETH from V2, get as much X from V1 as we can, repay V2 with minimum X, keep the rest!
     const arbitrageAmount = expandTo18Decimals(1)
@@ -80,13 +78,6 @@ describe('ExampleFlashSwap', () => {
     const WETHPairToken0 = await WETHPair.token0()
     const amount0 = WETHPairToken0 === WETHPartner.address ? BigNumber.from(0) : arbitrageAmount
     const amount1 = WETHPairToken0 === WETHPartner.address ? arbitrageAmount : BigNumber.from(0)
-    console.log("amount0: %s", amount0)
-    console.log("amount1: %s", amount1)
-    const [ _reserve0, _reserve1] = await WETHPair.getReserves(); // gas savings
-    console.log("_reserve0: %s, _reserve1: %s", _reserve0, _reserve1)
-    console.log("Token0: %s", await WETHPair.token0())
-    console.log("Token1: %s", await WETHPair.token1())
-    console.log("flashSwapExample: %s", flashSwapExample.address)
     await WETHPair.swap(
       amount0,
       amount1,
@@ -95,71 +86,72 @@ describe('ExampleFlashSwap', () => {
       overrides
     )
 
-    // const balanceAfter = await WETHPartner.balanceOf(wallet.address)
-    // const profit = balanceAfter.sub(balanceBefore).div(expandTo18Decimals(1))
-    // const reservesV1 = [
-    //   await WETHPartner.balanceOf(WETHExchangeV1.address),
-    //   await provider.getBalance(WETHExchangeV1.address)
-    // ]
-    // const priceV1 = reservesV1[0].div(reservesV1[1])
-    // const reservesV2 = (await WETHPair.getReserves()).slice(0, 2)
-    // const priceV2 = WETHPairToken0 === WETHPartner.address ? reservesV2[0].div(reservesV2[1]) : reservesV2[1].div(reservesV2[0])
+    const balanceAfter = await WETHPartner.balanceOf(wallet.address)
+    const profit = balanceAfter.sub(balanceBefore).div(expandTo18Decimals(1))
+    const reservesV1 = [
+      await WETHPartner.balanceOf(WETHExchangeV1.address),
+      await provider.getBalance(WETHExchangeV1.address)
+    ]
+    const priceV1 = reservesV1[0].div(reservesV1[1])
+    const reservesV2 = (await WETHPair.getReserves()).slice(0, 2)
+    const priceV2 =
+      WETHPairToken0 === WETHPartner.address ? reservesV2[0].div(reservesV2[1]) : reservesV2[1].div(reservesV2[0])
 
-    // expect(profit.toString()).to.eq('69') // our profit is ~69 tokens
-    // expect(priceV1.toString()).to.eq('165') // we pushed the v1 price down to ~165
-    // expect(priceV2.toString()).to.eq('123') // we pushed the v2 price up to ~123
+    expect(profit.toString()).to.eq('69') // our profit is ~69 tokens
+    expect(priceV1.toString()).to.eq('165') // we pushed the v1 price down to ~165
+    expect(priceV2.toString()).to.eq('123') // we pushed the v2 price up to ~123
   })
 
-  // it('uniswapV2Call:1', async () => {
-  //   // add liquidity to V1 at a rate of 1 ETH / 100 X
-  //   const WETHPartnerAmountV1 = expandTo18Decimals(1000)
-  //   const ETHAmountV1 = expandTo18Decimals(10)
-  //   await WETHPartner.approve(WETHExchangeV1.address, WETHPartnerAmountV1)
-  //   await WETHExchangeV1.addLiquidity(BigNumber.from(1), WETHPartnerAmountV1, MaxUint256, {
-  //     ...overrides,
-  //     value: ETHAmountV1
-  //   })
+  it('uniswapV2Call:1', async () => {
+    // add liquidity to V1 at a rate of 1 ETH / 100 X
+    const WETHPartnerAmountV1 = expandTo18Decimals(1000)
+    const ETHAmountV1 = expandTo18Decimals(10)
+    await WETHPartner.approve(WETHExchangeV1.address, WETHPartnerAmountV1)
+    await WETHExchangeV1.addLiquidity(BigNumber.from(1), WETHPartnerAmountV1, MaxUint256, {
+      ...overrides,
+      value: ETHAmountV1
+    })
 
-  //   // add liquidity to V2 at a rate of 1 ETH / 200 X
-  //   const WETHPartnerAmountV2 = expandTo18Decimals(2000)
-  //   const ETHAmountV2 = expandTo18Decimals(10)
-  //   await WETHPartner.transfer(WETHPair.address, WETHPartnerAmountV2)
-  //   await WETH.deposit({ value: ETHAmountV2 })
-  //   await WETH.transfer(WETHPair.address, ETHAmountV2)
-  //   await WETHPair.mint(wallet.address, overrides)
+    // add liquidity to V2 at a rate of 1 ETH / 200 X
+    const WETHPartnerAmountV2 = expandTo18Decimals(2000)
+    const ETHAmountV2 = expandTo18Decimals(10)
+    await WETHPartner.transfer(WETHPair.address, WETHPartnerAmountV2)
+    await WETH.deposit({ value: ETHAmountV2 })
+    await WETH.transfer(WETHPair.address, ETHAmountV2)
+    await WETHPair.mint(wallet.address, overrides)
 
-  //   const balanceBefore = await provider.getBalance(wallet.address)
+    const balanceBefore = await provider.getBalance(wallet.address)
 
-  //   // now, execute arbitrage via uniswapV2Call:
-  //   // receive 200 X from V2, get as much ETH from V1 as we can, repay V2 with minimum ETH, keep the rest!
-  //   const arbitrageAmount = expandTo18Decimals(200)
-  //   // instead of being 'hard-coded', the above value could be calculated optimally off-chain. this would be
-  //   // better, but it'd be better yet to calculate the amount at runtime, on-chain. unfortunately, this requires a
-  //   // swap-to-price calculation, which is a little tricky, and out of scope for the moment
-  //   const WETHPairToken0 = await WETHPair.token0()
-  //   const amount0 = WETHPairToken0 === WETHPartner.address ? arbitrageAmount : BigNumber.from(0)
-  //   const amount1 = WETHPairToken0 === WETHPartner.address ? BigNumber.from(0) : arbitrageAmount
-  //   await WETHPair.swap(
-  //     amount0,
-  //     amount1,
-  //     flashSwapExample.address,
-  //     defaultAbiCoder.encode(['uint'], [BigNumber.from(1)]),
-  //     overrides
-  //   )
+    // now, execute arbitrage via uniswapV2Call:
+    // receive 200 X from V2, get as much ETH from V1 as we can, repay V2 with minimum ETH, keep the rest!
+    const arbitrageAmount = expandTo18Decimals(200)
+    // instead of being 'hard-coded', the above value could be calculated optimally off-chain. this would be
+    // better, but it'd be better yet to calculate the amount at runtime, on-chain. unfortunately, this requires a
+    // swap-to-price calculation, which is a little tricky, and out of scope for the moment
+    const WETHPairToken0 = await WETHPair.token0()
+    const amount0 = WETHPairToken0 === WETHPartner.address ? arbitrageAmount : BigNumber.from(0)
+    const amount1 = WETHPairToken0 === WETHPartner.address ? BigNumber.from(0) : arbitrageAmount
+    await WETHPair.swap(
+      amount0,
+      amount1,
+      flashSwapExample.address,
+      defaultAbiCoder.encode(['uint'], [BigNumber.from(1)]),
+      overrides
+    )
 
-  //   const balanceAfter = await provider.getBalance(wallet.address)
-  //   const profit = balanceAfter.sub(balanceBefore)
-  //   const reservesV1 = [
-  //     await WETHPartner.balanceOf(WETHExchangeV1.address),
-  //     await provider.getBalance(WETHExchangeV1.address)
-  //   ]
-  //   const priceV1 = reservesV1[0].div(reservesV1[1])
-  //   const reservesV2 = (await WETHPair.getReserves()).slice(0, 2)
-  //   const priceV2 =
-  //     WETHPairToken0 === WETHPartner.address ? reservesV2[0].div(reservesV2[1]) : reservesV2[1].div(reservesV2[0])
+    const balanceAfter = await provider.getBalance(wallet.address)
+    const profit = balanceAfter.sub(balanceBefore)
+    const reservesV1 = [
+      await WETHPartner.balanceOf(WETHExchangeV1.address),
+      await provider.getBalance(WETHExchangeV1.address)
+    ]
+    const priceV1 = reservesV1[0].div(reservesV1[1])
+    const reservesV2 = (await WETHPair.getReserves()).slice(0, 2)
+    const priceV2 =
+      WETHPairToken0 === WETHPartner.address ? reservesV2[0].div(reservesV2[1]) : reservesV2[1].div(reservesV2[0])
 
-  //   expect(formatEther(profit)).to.eq('0.548043441089763649') // our profit is ~.5 ETH
-  //   expect(priceV1.toString()).to.eq('143') // we pushed the v1 price up to ~143
-  //   expect(priceV2.toString()).to.eq('161') // we pushed the v2 price down to ~161
-  // })
+    expect(formatEther(profit)).to.eq('0.548043441089763649') // our profit is ~.5 ETH
+    expect(priceV1.toString()).to.eq('143') // we pushed the v1 price up to ~143
+    expect(priceV2.toString()).to.eq('161') // we pushed the v2 price down to ~161
+  })
 })
