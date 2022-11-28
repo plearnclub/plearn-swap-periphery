005512ec0f63358bf669c3dc106a75aae8a6c4d4
Sitthiphong Muntrima <sitthiphong@ookbee.com>
2022-11-28 13:10:48 +0700

    Update RPC Url for chain Goerli and Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,13 +26,13 @@ export default {
       blockGasLimit: 99999999
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/",
+      url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       chainId: 5,
       accounts: { mnemonic: mnemonic },
       blockGasLimit: 99999999
     },
     sepolia: {
-      url: "https://sepolia.infura.io/v3/",
+      url: "https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       chainId: 11155111,
       accounts: { mnemonic: mnemonic },
       blockGasLimit: 99999999
